dcc3756080287fa0a039900f463c34e7bc09d55b
kyamamura <kyamamura@lexi.co.jp>
2020-04-13 11:49:21 +0900

    再帰下降構文解析を実装

--- 9cc.c
+++ 9cc.c
@@ -123,6 +123,92 @@ Token *tokenize(char *p)
 }
 
 
+typedef enum {
+	ND_ADD,
+	ND_SUB,
+	ND_MUL,
+	ND_DIV,
+	ND_NUM,
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node
+{
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+Node *new_node_num(int val)
+{
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *expr()
+{
+	Node *node = new_node_num(expect_number());
+
+	for (;;)
+	{
+		if (consume('+'))
+			node = new_node(ND_ADD, node, expr());
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, expr());
+		else 
+			return node;
+	}
+}
+
+void gen(Node *node)
+{
+	if (node->kind == ND_NUM)
+	{
+		printf("  push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+
+	switch (node->kind)
+	{
+	case ND_ADD:
+		printf("  add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("  sub rax, rdi\n");
+		break;
+	case ND_MUL:
+		printf("  imul rax, rdi\n");
+		break;
+	case ND_DIV:
+		printf("  cpo\n");
+		printf("  idiv rdi\n");
+		break;
+	}
+
+	printf("  push rax\n");
+}
+
+
+
 int main(int argc, char **argv)
 {
 	if (argc != 2)
@@ -133,26 +219,16 @@ int main(int argc, char **argv)
 	
 	user_input = argv[1];
 	token = tokenize(argv[1]);
+	Node *node = expr();
 
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
 
-	// 式の最初は数値.
-	printf("  mov rax, %d\n", expect_number());
+	gen(node);
 
-	while (!at_eof())
-	{
-		if (consume('+'))
-		{
-			printf("  add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf("  sub rax, %d\n", expect_number());
-	}
-
+	// スタックトップをRAXにロード.
+	printf("  pop rax\n");
 	printf("  ret\n");
 	return 0;
 }
