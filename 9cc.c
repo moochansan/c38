b00db5b3e0dba7406945b6ce7889b3129bde2b97
kyamamura <kyamamura@lexi.co.jp>
2020-04-14 10:00:08 +0900

    単項演算子(+-)を追加

--- 9cc.c
+++ 9cc.c
@@ -169,6 +169,7 @@ Node *new_node_num(int val)
 Node *expr(void);
 Node *mul(void);
 Node *primary(void);
+Node *unary(void);
 
 // expr = mul ("+" mul | "-" mul)*
 Node *expr()
@@ -186,17 +187,17 @@ Node *expr()
 	}
 }
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
 Node *mul()
 {
-	Node *node = primary();
+	Node *node = unary();
 
 	for (;;)
 	{
 		if (consume('*'))
-			node = new_node(ND_MUL, node, primary());
+			node = new_node(ND_MUL, node, unary());
 		else if (consume('/'))
-			node = new_node(ND_DIV, node, primary());
+			node = new_node(ND_DIV, node, unary());
 		else
 			return node;
 	}
@@ -213,6 +214,16 @@ Node *primary()
 	}
 	
 	return new_node_num(expect_number());
+}
+
+// unary = ("+" | "-")? primary
+Node *unary()
+{
+	if (consume('+'))
+		return primary();
+	if (consume('-'))
+		return new_node(ND_SUB, new_node_num(0), primary());
+	return primary();
 }
 
 
--- test.sh
+++ test.sh
@@ -22,9 +22,10 @@ assert 21 "5+20-4"
 assert 41 "12 + 34 - 5 "
 assert 11 "1 + 2 * 5"
 assert 13 "9 + 8 / 2"
-assert 10 "3*5-10/2"
 assert 7 "3+2*(3+5)/(8-4)"
 assert 7 "21-(3*(3-1)+1)*2"
+assert 10 "-10+20"
+assert 25 "(-5)*(-5)"
 
 echo "Passed :D"
 
